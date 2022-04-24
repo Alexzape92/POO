3a20d53f4209fc096e1cfe1e59d6c7171bc6073f
Alejandro Guitarte Fernandez <alexguitarte@gmail.com>
2022-04-24 17:56:41 +0200

    Clases Numero y Tarjeta terminadas

--- Guitarte_Fernandez_Alejandro/P2/tarjeta.cpp
+++ Guitarte_Fernandez_Alejandro/P2/tarjeta.cpp
@@ -45,3 +45,55 @@ caduc{fec}, act{true}{
 
 
 }
+
+Tarjeta::Tipo Tarjeta::tipo() const{
+    switch(num[0]){
+        case '3': if(num[1] == '4' || num[1] == '7') return Tipo{AmericanExpress}; else return Tipo{JCB}; break;
+        case '4': return Tipo{VISA}; break;
+        case '5': return Tipo{Mastercard}; break;
+        case '6': return Tipo{Maestro}; break;
+        default: return Tipo{Otro}; break;
+    }
+}
+
+Tarjeta::~Tarjeta(){
+    if(titu != nullptr)
+        const_cast<Usuario*>(titu)->no_es_titular_de(*this);
+}
+
+//Operador de insercion en flujo de salida
+//Operadores de insercion en flujo
+std::ostream& operator <<(std::ostream& os, const Tarjeta::Tipo& t){
+    static Cadena cadenas[] {"Otro", "VISA", "Mastercard", "Maestro", "JCB", "American Express"};
+
+    return os << cadenas[t];
+}
+
+std::ostream& operator <<(std::ostream& os, const Tarjeta& tj){
+    os << tj.tipo() << std::endl;
+    os << tj.numero() << std::endl;
+
+    Cadena nombre{tj.titular()->nombre().c_str()}, apellidos{tj.titular()->apellidos().c_str()};  //Guardamos las cadenas en cuestion
+    for(auto i = nombre.begin(); i != nombre.end(); i++)
+        *i = toupper((unsigned char) *i);
+    for(auto i = apellidos.begin(); i != apellidos.end(); i++)
+        *i = toupper((unsigned char) *i);
+
+    os << nombre << " " << apellidos << std::endl;
+
+    if(tj.caducidad().mes() < 10)
+        os << "0" << tj.caducidad().mes();
+    else
+        os << tj.caducidad().mes();
+    if(tj.caducidad().anno() % 1000 < 10)
+        os << "/0" << tj.caducidad().anno() % 1000 << std::endl;
+    else
+        os << "/" << tj.caducidad().anno() % 1000 << std::endl;
+    
+    return os;
+}
+
+//Comparacion
+bool operator <(const Tarjeta& tj1, const Tarjeta& tj2){
+    return tj1.numero() < tj2.numero();
+}
